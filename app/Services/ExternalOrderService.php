3995d9a89024502ae5c9fd40c7d0cc276f082590
Radu Soltan <radusoltan@gmail.com>
2024-12-27 21:14:08 +0200

    Update ExternalOrderService.php
    add env to service

--- app/Services/ExternalOrderService.php
+++ app/Services/ExternalOrderService.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Http;
 class ExternalOrderService {
 
     public function getOrderStatus(string $orderNumber){
-        $response = Http::get("http://localhost:8000/api/orders/{$orderNumber}");
+        $response = Http::get(env('EXTERNAL_API_URL')."api/orders/{$orderNumber}");
 
         if ($response->successful()) {
             return $response->json()['data'];
