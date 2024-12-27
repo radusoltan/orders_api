11ad40f720cfc9439f256dc3af89da79b4694371
Radu Soltan <radusoltan@gmail.com>
2024-12-27 21:15:29 +0200

    Update services.php
    some updates

--- config/services.php
+++ config/services.php
@@ -34,5 +34,8 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    'external_api' => [
+        'external_api_url' => env('EXTERNEL_API_URL')
+    ]
 
 ];
